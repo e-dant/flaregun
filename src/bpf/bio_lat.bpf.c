1fa2add567f2a648d0908cb4bd9f58968b32b8a9
Will <edant.io@proton.me>
2024-08-24 16:23:52 -0400

    clang-format

--- src/bpf/bio_lat.bpf.c
+++ src/bpf/bio_lat.bpf.c
@@ -1,30 +1,28 @@
 // SPDX-License-Identifier: GPL-2.0
 // Copyright (c) 2020 Wenbo Zhang
-#include "vmlinux.h"
 #include "constants.h"
-#include <bpf/bpf_helpers.h>
+#include "core_fixes.bpf.h"
+#include "vmlinux.h"
 #include <bpf/bpf_core_read.h>
+#include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
-
-#include "core_fixes.bpf.h"
-
-#define MAX_ENTRIES	10240
+#define MAX_ENTRIES 10240
 #define DISK_NAME_LEN 32
-#define RWBS_LEN  8
+#define RWBS_LEN 8
 
 #define MINORBITS 20
 #define MINORMASK ((1U << MINORBITS) - 1)
 
 #define MKDEV(ma, mi) (((ma) << MINORBITS) | (mi))
 
-const volatile bool targ_filter_cgroup = false;
-const volatile bool targ_filter_queued = false;
-const volatile bool targ_filter_dev = false;
-const volatile __u32 targ_dev = 0;
-const volatile pid_t targ_pid = 0;
-const volatile pid_t targ_tgid = 0;
-const volatile __u64 min_lat_us = 0;
+volatile bool const targ_filter_cgroup = false;
+volatile bool const targ_filter_queued = false;
+volatile bool const targ_filter_dev = false;
+volatile const __u32 targ_dev = 0;
+volatile const pid_t targ_pid = 0;
+volatile const pid_t targ_tgid = 0;
+volatile const __u64 min_lat_us = 0;
 
 struct event {
   u8 task[FL_TASK_COMM_LEN];
@@ -41,38 +39,37 @@ struct event {
 extern __u32 LINUX_KERNEL_VERSION __kconfig;
 
 struct {
-	__uint(type, BPF_MAP_TYPE_CGROUP_ARRAY);
-	__type(key, u32);
-	__type(value, u32);
-	__uint(max_entries, 1);
+  __uint(type, BPF_MAP_TYPE_CGROUP_ARRAY);
+  __type(key, u32);
+  __type(value, u32);
+  __uint(max_entries, 1);
 } cgroup_map SEC(".maps");
 
 struct piddata {
-	u32 pid;
+  u32 pid;
   u32 tgid;
 };
 
 struct stage {
-	u64 insert;
-	u64 issue;
-	__u32 dev;
+  u64 insert;
+  u64 issue;
+  __u32 dev;
 };
 
 struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, MAX_ENTRIES);
-	__type(key, struct request *);
-	__type(value, struct stage);
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, MAX_ENTRIES);
+  __type(key, struct request*);
+  __type(value, struct stage);
 } start SEC(".maps");
 
 struct {
-	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-	__uint(key_size, sizeof(u32));
-	__uint(value_size, sizeof(u32));
+  __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+  __uint(key_size, sizeof(u32));
+  __uint(value_size, sizeof(u32));
 } events SEC(".maps");
 
-static __always_inline
-int current_piddata(struct request *rq, struct piddata *piddata)
+static __always_inline int current_piddata(struct request* rq, struct piddata* piddata)
 {
   u64 pid_tgid = bpf_get_current_pid_tgid();
   u32 tgid = (u32)pid_tgid;
@@ -85,108 +82,107 @@ int current_piddata(struct request *rq, struct piddata *piddata)
   return 0;
 }
 
-static __always_inline
-int trace_rq_start(struct request *rq, bool insert)
+static __always_inline int trace_rq_start(struct request* rq, bool insert)
 {
-	struct stage *stagep, stage = {};
-	u64 ts = bpf_ktime_get_ns();
+  struct stage *stagep, stage = {};
+  u64 ts = bpf_ktime_get_ns();
 
-	stagep = bpf_map_lookup_elem(&start, &rq);
-	if (!stagep) {
-		struct gendisk *disk = get_disk(rq);
+  stagep = bpf_map_lookup_elem(&start, &rq);
+  if (! stagep) {
+    struct gendisk* disk = get_disk(rq);
 
-		stage.dev = disk ? MKDEV(BPF_CORE_READ(disk, major),
-				BPF_CORE_READ(disk, first_minor)) : 0;
-		if (targ_filter_dev && targ_dev != stage.dev)
-			return 0;
-		stagep = &stage;
-	}
-	if (insert)
-		stagep->insert = ts;
-	else
-		stagep->issue = ts;
-	if (stagep == &stage) {
-		bpf_map_update_elem(&start, &rq, stagep, 0);
+    stage.dev = disk ? MKDEV(BPF_CORE_READ(disk, major), BPF_CORE_READ(disk, first_minor)) : 0;
+    if (targ_filter_dev && targ_dev != stage.dev)
+      return 0;
+    stagep = &stage;
   }
-	return 0;
+  if (insert)
+    stagep->insert = ts;
+  else
+    stagep->issue = ts;
+  if (stagep == &stage) {
+    bpf_map_update_elem(&start, &rq, stagep, 0);
+  }
+  return 0;
 }
 
 SEC("tp_btf/block_rq_insert")
+
 int BPF_PROG(block_rq_insert)
 {
-	if (targ_filter_cgroup && !bpf_current_task_under_cgroup(&cgroup_map, 0))
-		return 0;
+  if (targ_filter_cgroup && ! bpf_current_task_under_cgroup(&cgroup_map, 0))
+    return 0;
 
-	/**
-	 * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-	 * from TP_PROTO(struct request_queue *q, struct request *rq)
-	 * to TP_PROTO(struct request *rq)
-	 */
-	if (LINUX_KERNEL_VERSION >= KERNEL_VERSION(5, 11, 0))
-		return trace_rq_start((void *)ctx[0], true);
-	else
-		return trace_rq_start((void *)ctx[1], true);
+  /**
+   * commit a54895fa (v5.11-rc1) changed tracepoint argument list
+   * from TP_PROTO(struct request_queue *q, struct request *rq)
+   * to TP_PROTO(struct request *rq)
+   */
+  if (LINUX_KERNEL_VERSION >= KERNEL_VERSION(5, 11, 0))
+    return trace_rq_start((void*)ctx[0], true);
+  else
+    return trace_rq_start((void*)ctx[1], true);
 }
 
 SEC("tp_btf/block_rq_issue")
+
 int BPF_PROG(block_rq_issue)
 {
-	if (targ_filter_cgroup && !bpf_current_task_under_cgroup(&cgroup_map, 0))
-		return 0;
+  if (targ_filter_cgroup && ! bpf_current_task_under_cgroup(&cgroup_map, 0))
+    return 0;
 
-	/**
-	 * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-	 * from TP_PROTO(struct request_queue *q, struct request *rq)
-	 * to TP_PROTO(struct request *rq)
-	 */
-	if (LINUX_KERNEL_VERSION >= KERNEL_VERSION(5, 11, 0))
-		return trace_rq_start((void *)ctx[0], false);
-	else
-		return trace_rq_start((void *)ctx[1], false);
+  /**
+   * commit a54895fa (v5.11-rc1) changed tracepoint argument list
+   * from TP_PROTO(struct request_queue *q, struct request *rq)
+   * to TP_PROTO(struct request *rq)
+   */
+  if (LINUX_KERNEL_VERSION >= KERNEL_VERSION(5, 11, 0))
+    return trace_rq_start((void*)ctx[0], false);
+  else
+    return trace_rq_start((void*)ctx[1], false);
 }
 
 SEC("tp_btf/block_rq_complete")
-int BPF_PROG(block_rq_complete, struct request *rq, int error,
-	     unsigned int nr_bytes)
+
+int BPF_PROG(block_rq_complete, struct request* rq, int error, unsigned int nr_bytes)
 {
-	if (targ_filter_cgroup && !bpf_current_task_under_cgroup(&cgroup_map, 0))
-		return 0;
+  if (targ_filter_cgroup && ! bpf_current_task_under_cgroup(&cgroup_map, 0))
+    return 0;
 
-	u64 ts = bpf_ktime_get_ns();
-	struct event event = {};
-	struct stage *stagep;
-	s64 delta;
+  u64 ts = bpf_ktime_get_ns();
+  struct event event = {};
+  struct stage* stagep;
+  s64 delta;
 
-	stagep = bpf_map_lookup_elem(&start, &rq);
-	if (!stagep)
-		return 0;
-	delta = (s64)(ts - stagep->issue);
+  stagep = bpf_map_lookup_elem(&start, &rq);
+  if (! stagep)
+    return 0;
+  delta = (s64)(ts - stagep->issue);
   u64 delta_us = delta / 1000ul;
-	if (delta < 0 || delta_us < min_lat_us)
-		goto cleanup;
+  if (delta < 0 || delta_us < min_lat_us)
+    goto cleanup;
   struct piddata piddata = {};
   if (current_piddata(rq, &piddata))
     goto cleanup;
   bpf_get_current_comm(&event.task, sizeof(event.task));
   event.pid = piddata.pid;
-	event.lat_us = delta_us;
-	if (targ_filter_queued && BPF_CORE_READ(rq, q, elevator)) {
-		if (!stagep->insert)
-			event.q_lat_us = -1; /* missed or don't insert entry */
-		else
-			event.q_lat_us = (stagep->issue - stagep->insert) / 1000;
-	}
-	event.ts = ts;
-	event.sector = BPF_CORE_READ(rq, __sector);
-	event.len = BPF_CORE_READ(rq, __data_len);
-	event.cmd_flags = BPF_CORE_READ(rq, cmd_flags);
-	event.dev = stagep->dev;
-	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event,
-			sizeof(event));
+  event.lat_us = delta_us;
+  if (targ_filter_queued && BPF_CORE_READ(rq, q, elevator)) {
+    if (! stagep->insert)
+      event.q_lat_us = -1; /* missed or don't insert entry */
+    else
+      event.q_lat_us = (stagep->issue - stagep->insert) / 1000;
+  }
+  event.ts = ts;
+  event.sector = BPF_CORE_READ(rq, __sector);
+  event.len = BPF_CORE_READ(rq, __data_len);
+  event.cmd_flags = BPF_CORE_READ(rq, cmd_flags);
+  event.dev = stagep->dev;
+  bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &event, sizeof(event));
 
 cleanup:
-	bpf_map_delete_elem(&start, &rq);
-	return 0;
+  bpf_map_delete_elem(&start, &rq);
+  return 0;
 }
 
 char LICENSE[] SEC("license") = "GPL";
--- src/bpf/core_fixes.bpf.h
+++ src/bpf/core_fixes.bpf.h
@@ -14,20 +14,20 @@
  *     https://github.com/torvalds/linux/commit/2f064a59a1
  */
 struct task_struct___o {
-	volatile long int state;
+  volatile long int state;
 } __attribute__((preserve_access_index));
 
 struct task_struct___x {
-	unsigned int __state;
+  unsigned int __state;
 } __attribute__((preserve_access_index));
 
-static __always_inline __s64 get_task_state(void *task)
+static __always_inline __s64 get_task_state(void* task)
 {
-	struct task_struct___x *t = task;
+  struct task_struct___x* t = task;
 
-	if (bpf_core_field_exists(t->__state))
-		return BPF_CORE_READ(t, __state);
-	return BPF_CORE_READ((struct task_struct___o *)task, state);
+  if (bpf_core_field_exists(t->__state))
+    return BPF_CORE_READ(t, __state);
+  return BPF_CORE_READ((struct task_struct___o*)task, state);
 }
 
 /**
@@ -37,20 +37,20 @@ static __always_inline __s64 get_task_state(void *task)
  *     https://github.com/torvalds/linux/commit/309dca309fc3
  */
 struct bio___o {
-	struct gendisk *bi_disk;
+  struct gendisk* bi_disk;
 } __attribute__((preserve_access_index));
 
 struct bio___x {
-	struct block_device *bi_bdev;
+  struct block_device* bi_bdev;
 } __attribute__((preserve_access_index));
 
-static __always_inline struct gendisk *get_gendisk(void *bio)
+static __always_inline struct gendisk* get_gendisk(void* bio)
 {
-	struct bio___x *b = bio;
+  struct bio___x* b = bio;
 
-	if (bpf_core_field_exists(b->bi_bdev))
-		return BPF_CORE_READ(b, bi_bdev, bd_disk);
-	return BPF_CORE_READ((struct bio___o *)bio, bi_disk);
+  if (bpf_core_field_exists(b->bi_bdev))
+    return BPF_CORE_READ(b, bi_bdev, bd_disk);
+  return BPF_CORE_READ((struct bio___o*)bio, bi_disk);
 }
 
 /**
@@ -63,22 +63,22 @@ static __always_inline struct gendisk *get_gendisk(void *bio)
  *     https://github.com/torvalds/linux/commit/d5869fdc189f
  */
 struct trace_event_raw_block_rq_complete___x {
-	dev_t dev;
-	sector_t sector;
-	unsigned int nr_sector;
+  dev_t dev;
+  sector_t sector;
+  unsigned int nr_sector;
 } __attribute__((preserve_access_index));
 
 struct trace_event_raw_block_rq_completion___x {
-	dev_t dev;
-	sector_t sector;
-	unsigned int nr_sector;
+  dev_t dev;
+  sector_t sector;
+  unsigned int nr_sector;
 } __attribute__((preserve_access_index));
 
 static __always_inline bool has_block_rq_completion()
 {
-	if (bpf_core_type_exists(struct trace_event_raw_block_rq_completion___x))
-		return true;
-	return false;
+  if (bpf_core_type_exists(struct trace_event_raw_block_rq_completion___x))
+    return true;
+  return false;
 }
 
 /**
@@ -92,21 +92,21 @@ static __always_inline bool has_block_rq_completion()
  *     https://github.com/torvalds/linux/commit/f3fa33acca9f
  */
 struct request_queue___x {
-	struct gendisk *disk;
+  struct gendisk* disk;
 } __attribute__((preserve_access_index));
 
 struct request___x {
-	struct request_queue___x *q;
-	struct gendisk *rq_disk;
+  struct request_queue___x* q;
+  struct gendisk* rq_disk;
 } __attribute__((preserve_access_index));
 
-static __always_inline struct gendisk *get_disk(void *request)
+static __always_inline struct gendisk* get_disk(void* request)
 {
-	struct request___x *r = request;
+  struct request___x* r = request;
 
-	if (bpf_core_field_exists(r->rq_disk))
-		return BPF_CORE_READ(r, rq_disk);
-	return BPF_CORE_READ(r, q, disk);
+  if (bpf_core_field_exists(r->rq_disk))
+    return BPF_CORE_READ(r, rq_disk);
+  return BPF_CORE_READ(r, q, disk);
 }
 
 /**
@@ -125,22 +125,22 @@ static __always_inline struct gendisk *get_disk(void *request)
  *     https://github.com/torvalds/linux/commit/abf08576afe3
  */
 struct renamedata___x {
-	struct user_namespace *old_mnt_userns;
-	struct new_mnt_idmap *new_mnt_idmap;
+  struct user_namespace* old_mnt_userns;
+  struct new_mnt_idmap* new_mnt_idmap;
 } __attribute__((preserve_access_index));
 
 static __always_inline bool renamedata_has_old_mnt_userns_field(void)
 {
-	if (bpf_core_field_exists(struct renamedata___x, old_mnt_userns))
-		return true;
-	return false;
+  if (bpf_core_field_exists(struct renamedata___x, old_mnt_userns))
+    return true;
+  return false;
 }
 
 static __always_inline bool renamedata_has_new_mnt_idmap_field(void)
 {
-	if (bpf_core_field_exists(struct renamedata___x, new_mnt_idmap))
-		return true;
-	return false;
+  if (bpf_core_field_exists(struct renamedata___x, new_mnt_idmap))
+    return true;
+  return false;
 }
 
 /**
@@ -153,29 +153,29 @@ static __always_inline bool renamedata_has_new_mnt_idmap_field(void)
  */
 
 struct trace_event_raw_kmem_free___x {
-	const void *ptr;
+  void const* ptr;
 } __attribute__((preserve_access_index));
 
 struct trace_event_raw_kfree___x {
-	const void *ptr;
+  void const* ptr;
 } __attribute__((preserve_access_index));
 
 struct trace_event_raw_kmem_cache_free___x {
-	const void *ptr;
+  void const* ptr;
 } __attribute__((preserve_access_index));
 
 static __always_inline bool has_kfree()
 {
-	if (bpf_core_type_exists(struct trace_event_raw_kfree___x))
-		return true;
-	return false;
+  if (bpf_core_type_exists(struct trace_event_raw_kfree___x))
+    return true;
+  return false;
 }
 
 static __always_inline bool has_kmem_cache_free()
 {
-	if (bpf_core_type_exists(struct trace_event_raw_kmem_cache_free___x))
-		return true;
-	return false;
+  if (bpf_core_type_exists(struct trace_event_raw_kmem_cache_free___x))
+    return true;
+  return false;
 }
 
 /**
@@ -186,15 +186,15 @@ static __always_inline bool has_kmem_cache_free()
  *    https://github.com/torvalds/linux/commit/11e9734bcb6a
  */
 struct trace_event_raw_kmem_alloc_node___x {
-	const void *ptr;
-	size_t bytes_alloc;
+  void const* ptr;
+  size_t bytes_alloc;
 } __attribute__((preserve_access_index));
 
 static __always_inline bool has_kmem_alloc_node(void)
 {
-	if (bpf_core_type_exists(struct trace_event_raw_kmem_alloc_node___x))
-		return true;
-	return false;
+  if (bpf_core_type_exists(struct trace_event_raw_kmem_alloc_node___x))
+    return true;
+  return false;
 }
 
 /**
@@ -206,103 +206,104 @@ static __always_inline bool has_kmem_alloc_node(void)
  *    https://github.com/torvalds/linux/commit/2c1d697fb8ba
  */
 struct trace_event_raw_kmem_alloc___x {
-	const void *ptr;
-	size_t bytes_alloc;
+  void const* ptr;
+  size_t bytes_alloc;
 } __attribute__((preserve_access_index));
 
 struct trace_event_raw_kmalloc___x {
-	const void *ptr;
-	size_t bytes_alloc;
+  void const* ptr;
+  size_t bytes_alloc;
 } __attribute__((preserve_access_index));
 
 struct trace_event_raw_kmem_cache_alloc___x {
-	const void *ptr;
-	size_t bytes_alloc;
+  void const* ptr;
+  size_t bytes_alloc;
 } __attribute__((preserve_access_index));
 
 static __always_inline bool has_kmem_alloc(void)
 {
-	if (bpf_core_type_exists(struct trace_event_raw_kmem_alloc___x))
-		return true;
-	return false;
+  if (bpf_core_type_exists(struct trace_event_raw_kmem_alloc___x))
+    return true;
+  return false;
 }
 
 /**
  * The bpf_get_socket_cookie helper is landed since kernel v4.12，
  * but only available for tracing programs since kernel v5.12
- * via commit c5dbb89fc2ac("bpf: Expose bpf_get_socket_cookie to tracing programs").
- * Since the helper is used to provide a unique socket identifier,
- * we could use the sock itself as the identifier if the helper is not available.
- * Here, we use BPF_FUNC_check_mtu to check the availability of the helper
- * since they are both introduced in v5.12.
+ * via commit c5dbb89fc2ac("bpf: Expose bpf_get_socket_cookie to tracing
+ * programs"). Since the helper is used to provide a unique socket identifier,
+ * we could use the sock itself as the identifier if the helper is not
+ * available. Here, we use BPF_FUNC_check_mtu to check the availability of the
+ * helper since they are both introduced in v5.12.
  *
  * see:
  *    https://github.com/torvalds/linux/commit/91b8270f2a4d
  *    https://github.com/torvalds/linux/commit/c5dbb89fc2ac
  *    https://github.com/torvalds/linux/commit/34b2021cc616
  */
-static __always_inline __u64 get_sock_ident(struct sock *sk)
+static __always_inline __u64 get_sock_ident(struct sock* sk)
 {
-	if (bpf_core_enum_value_exists(enum bpf_func_id, BPF_FUNC_check_mtu)) {
-		return bpf_get_socket_cookie(sk);
-	}
-	return (__u64)sk;
+  if (bpf_core_enum_value_exists(enum bpf_func_id, BPF_FUNC_check_mtu)) {
+    return bpf_get_socket_cookie(sk);
+  }
+  return (__u64)sk;
 }
 
 /**
- * During kernel 6.6 development cycle, several bitfields in struct inet_sock gone,
- * they are placed in inet_sock::inet_flags instead ([0]).
+ * During kernel 6.6 development cycle, several bitfields in struct inet_sock
+ * gone, they are placed in inet_sock::inet_flags instead ([0]).
  *
  * References:
- *   [0]: https://lore.kernel.org/all/20230816081547.1272409-1-edumazet@google.com/
+ *   [0]:
+ * https://lore.kernel.org/all/20230816081547.1272409-1-edumazet@google.com/
  */
 struct inet_sock___o {
-	__u8 freebind: 1;
-	__u8 transparent: 1;
-	__u8 bind_address_no_port: 1;
+  __u8 freebind             : 1;
+  __u8 transparent          : 1;
+  __u8 bind_address_no_port : 1;
 };
 
 enum {
-	INET_FLAGS_FREEBIND___x = 11,
-	INET_FLAGS_TRANSPARENT___x = 15,
-	INET_FLAGS_BIND_ADDRESS_NO_PORT___x = 18,
+  INET_FLAGS_FREEBIND___x = 11,
+  INET_FLAGS_TRANSPARENT___x = 15,
+  INET_FLAGS_BIND_ADDRESS_NO_PORT___x = 18,
 };
 
 struct inet_sock___x {
-	unsigned long inet_flags;
+  unsigned long inet_flags;
 };
 
-static __always_inline __u8 get_inet_sock_freebind(void *inet_sock)
+static __always_inline __u8 get_inet_sock_freebind(void* inet_sock)
 {
-	unsigned long inet_flags;
+  unsigned long inet_flags;
 
-	if (bpf_core_field_exists(struct inet_sock___o, freebind))
-		return BPF_CORE_READ_BITFIELD_PROBED((struct inet_sock___o *)inet_sock, freebind);
+  if (bpf_core_field_exists(struct inet_sock___o, freebind))
+    return BPF_CORE_READ_BITFIELD_PROBED((struct inet_sock___o*)inet_sock, freebind);
 
-	inet_flags = BPF_CORE_READ((struct inet_sock___x *)inet_sock, inet_flags);
-	return (1 << INET_FLAGS_FREEBIND___x) & inet_flags ? 1 : 0;
+  inet_flags = BPF_CORE_READ((struct inet_sock___x*)inet_sock, inet_flags);
+  return (1 << INET_FLAGS_FREEBIND___x) & inet_flags ? 1 : 0;
 }
 
-static __always_inline __u8 get_inet_sock_transparent(void *inet_sock)
+static __always_inline __u8 get_inet_sock_transparent(void* inet_sock)
 {
-	unsigned long inet_flags;
+  unsigned long inet_flags;
 
-	if (bpf_core_field_exists(struct inet_sock___o, transparent))
-		return BPF_CORE_READ_BITFIELD_PROBED((struct inet_sock___o *)inet_sock, transparent);
+  if (bpf_core_field_exists(struct inet_sock___o, transparent))
+    return BPF_CORE_READ_BITFIELD_PROBED((struct inet_sock___o*)inet_sock, transparent);
 
-	inet_flags = BPF_CORE_READ((struct inet_sock___x *)inet_sock, inet_flags);
-	return (1 << INET_FLAGS_TRANSPARENT___x) & inet_flags ? 1 : 0;
+  inet_flags = BPF_CORE_READ((struct inet_sock___x*)inet_sock, inet_flags);
+  return (1 << INET_FLAGS_TRANSPARENT___x) & inet_flags ? 1 : 0;
 }
 
-static __always_inline __u8 get_inet_sock_bind_address_no_port(void *inet_sock)
+static __always_inline __u8 get_inet_sock_bind_address_no_port(void* inet_sock)
 {
-	unsigned long inet_flags;
+  unsigned long inet_flags;
 
-	if (bpf_core_field_exists(struct inet_sock___o, bind_address_no_port))
-		return BPF_CORE_READ_BITFIELD_PROBED((struct inet_sock___o *)inet_sock, bind_address_no_port);
+  if (bpf_core_field_exists(struct inet_sock___o, bind_address_no_port))
+    return BPF_CORE_READ_BITFIELD_PROBED((struct inet_sock___o*)inet_sock, bind_address_no_port);
 
-	inet_flags = BPF_CORE_READ((struct inet_sock___x *)inet_sock, inet_flags);
-	return (1 << INET_FLAGS_BIND_ADDRESS_NO_PORT___x) & inet_flags ? 1 : 0;
+  inet_flags = BPF_CORE_READ((struct inet_sock___x*)inet_sock, inet_flags);
+  return (1 << INET_FLAGS_BIND_ADDRESS_NO_PORT___x) & inet_flags ? 1 : 0;
 }
 
 #endif /* __CORE_FIXES_BPF_H */
